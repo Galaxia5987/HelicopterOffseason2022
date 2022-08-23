e1bf1ebb6f57f7efca1ba5e62736410ae8821c98
Emma03 <emmalivne.03@gmail.com>
2022-08-23 16:57:29 +0300

    Add Subsystem

--- src/main/java/frc/robot/subsystems/Helicopter.java
+++ src/main/java/frc/robot/subsystems/Helicopter.java
@@ -1,2 +1,7 @@
-package frc.robot.subsystems;public class Helicopter {
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Helicopter extends SubsystemBase {
+    private final 
 }
