b5c469b0a7bb54dbfed8ef98e5d3e131c4fbff0b
lotan23 <jonathan.lotan@gmail.com>
2022-08-23 18:52:27 +0300

    you stoopid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,7 +14,7 @@ import frc.robot.subsystems.Choppa;
  *
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
- */
+ */ 
 public final class Constants {
 public static class Choppa{
     public static double kP = 0;
