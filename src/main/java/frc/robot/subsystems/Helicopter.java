741593a9322601978a66aeaddbe424b8189d4401
omer <omeryakoby@gmail.com>
2022-09-02 13:01:13 +0300

    add falcon

--- src/main/java/frc/robot/subsystems/Helicopter.java
+++ src/main/java/frc/robot/subsystems/Helicopter.java
@@ -5,14 +5,17 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Ports;
 
 public class Helicopter extends SubsystemBase {
-    private final WPI_TalonFX motor = new WPI_TalonFX(Ports.MOTOR);
+    private final WPI_TalonFX aux_motor = new WPI_TalonFX(Ports.MOTOR);
+
+    private final WPI_TalonFX main_motor = new WPI_TalonFX(Ports.MOTOR);
 
     private static Helicopter INSTANCE = null;
 
     public Helicopter(){
-        motor.setInverted(Ports.INVERT);
-        motor.enableVoltageCompensation(Ports.ENABLE_VOLTAGE_COMPENSATION);
-        motor.configVoltageCompSaturation(Ports.CONFIG_VOLTAGE_COMPSATURATION);
+        main_motor.setInverted(Ports.INVERT);
+        main_motor.enableVoltageCompensation(Ports.ENABLE_VOLTAGE_COMPENSATION);
+        main_motor.configVoltageCompSaturation(Ports.CONFIG_VOLTAGE_COMPSATURATION);
+        aux_motor.follow(main_motor);
     }
 
     /**
@@ -20,11 +23,11 @@ public class Helicopter extends SubsystemBase {
      * @param power
      */
     public void setPower(double power){
-       motor.set(power);
+       main_motor.set(power);
     }
 
     public double getPower(){
-        return motor.get();
+        return main_motor.get();
     }
 
     public static Helicopter getINSTANCE(){
